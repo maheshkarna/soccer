b4064e35622971b7790ae31b90e2f879fc5a28cf
LAPTOP-9GHUJEEE\mahes <mahesh@spondias.com>
2024-03-26 12:34:19 +0530

    my First Commit

---
+++ TopScore/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using log4net;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TopScore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+       
+
+    }
+
+}
